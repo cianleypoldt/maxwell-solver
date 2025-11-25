546379ec66dadf1ee9a547fcc3b1970c7b8235e8
cianleypoldt <cian@archlinux>
2025-11-25 16:40:33 +0100

    mathlibc separation

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include "maxwell-solver.h"
 #include "utils.h"
 
-#include <stdio.h>
-
 int main() {
-        simctx* sim = init_simulation(100, 100, 100, 0.1);
-        start_renderer(800, 600);
-
-        while (!should_close()) {
-                draw();
-        }
-
-        quit_renderer();
-        destroy_simulation(sim);
+        f64 mat[16];
+        f64 axis[] = { 1, 2, 3 };
+        v3to_cross_product3x3(mat, axis);
+        printNxN_named(mat, 4, 4, "should be identity!");
+        // simctx* sim = init_simulation(100, 100, 100, 0.1);
+        //
+        // start_renderer(800, 600);
+        // while (!should_close()) {
+        //         draw();
+        // }
+        //
+        // quit_renderer();
+        // destroy_simulation(sim);
 }
